504d3c5a05c52441104cdace4830ca9fdc57c0fc
jeremyraymond <jeremy.r.raymond@gmail.com>
2016-01-20 01:46:50 -0500

    Added PHPUnit and a couple classes
    I added PHPUnit to the project and I added a basic class, and then a
    basic test class that extends phpunit

---
+++ app/BasicMath.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jeremy
+ * Date: 1/20/2016
+ * Time: 1:24 AM
+ */
+class BasicMath
+{
+    function addition($numberone, $numbertwo) {
+        return $numberone + $numbertwo;
+    }
+    function subtraction($numberone, $numbertwo) {
+        return $numberone - $numbertwo;
+    }
+}
---
+++ tests/BasicMathTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jeremy
+ * Date: 1/20/2016
+ * Time: 1:32 AM
+ */
+
+class BasicMathTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp(){ }
+    public function tearDown(){ }
+}
