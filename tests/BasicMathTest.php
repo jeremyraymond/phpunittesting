275435408535386c950a9e277e72c69d06f30809
jeremyraymond <jeremy.r.raymond@gmail.com>
2016-01-20 02:33:11 -0500

    Created successful test of BasicMath class
    Created successful test of BasicMath class

--- tests/BasicMathTest.php
+++ tests/BasicMathTest.php
@@ -6,9 +6,15 @@
  * Date: 1/20/2016
  * Time: 1:32 AM
  */
+include('../app/BasicMath.php');
 
 class BasicMathTest extends PHPUnit_Framework_TestCase
 {
     public function setUp(){ }
     public function tearDown(){ }
+
+    public function testAddition() {
+        $math = new BasicMath();
+        $this->assertTrue($math->addition(2,2) == 4);
+    }
 }
